f28be6c8a93ffd775a50a04f2cebe12304c06322
Rakesh Barthipaka <you@example.com>
2023-11-21 22:20:37 +0530

    chenged

--- api.js
+++ api.js
@@ -33,19 +33,6 @@ app.get('/employee/:id', (req, res)=>{
 
 
 
-app.post('/users', (req, res)=> {
-    const user = req.body;
-    let insertQuery = `insert into users(id, firstname, lastname, location) 
-                       values(${user.id}, '${user.firstname}', '${user.lastname}', '${user.location}')`
-
-    client.query(insertQuery, (err, result)=>{
-        if(!err){
-            res.send('Insertion was successful')
-        }
-        else{ console.log(err.message) }
-    })
-    client.end;
-})
 
 // Assuming you have required necessary modules and initialized your express app and database client ('client')
 
